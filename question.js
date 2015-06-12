10cf3ae1f938917d01fb8e47fe26a9dd6b7c481f
jacky7398 <jackychu7398@gmail.com>
2015-06-12 22:39:27 +0000

    v2

--- question.js
+++ question.js
@@ -1,15 +1,31 @@
+debugger
 var submitButton = document.getElementById('submit');
-var messages = document.getElementById('logs');
+var messages = document.getElementById('comments');
+var myDataRef = new Firebase('https://rjmxjqj45hk.firebaseio-demo.com/');
 
 var addComment = function() {
   debugger
  
   var message = document.getElementById('message').value;
-  var sentence = message;
+  debugger
+  myDataRef.push({message: message});
+  
+};
+  
+myDataRef.on('child_added', function(data) {
+  debugger
+  
+  var message = data.val();
+  displayComments(message.message);
+});
+  
+function displayComments(message){
+  debugger
+  
   var listItem = document.createElement("p");
   listItem.textContent = message;
   messages.appendChild(listItem);
 
-};
+}
 
 submitButton.onclick = addComment;
