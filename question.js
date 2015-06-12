3752ce07411a28da60ca63bb52aacec8888e7398
jacky7398 <jackychu7398@gmail.com>
2015-06-12 23:11:42 +0000

    v2

--- question.js
+++ question.js
@@ -1,26 +1,21 @@
-debugger
 var submitButton = document.getElementById('submit');
 var messages = document.getElementById('comments');
 var myDataRef = new Firebase('https://rjmxjqj45hk.firebaseio-demo.com/');
 
 var addComment = function() {
-  debugger
  
   var message = document.getElementById('message').value;
-  debugger
   myDataRef.push({message: message});
   
 };
   
 myDataRef.on('child_added', function(data) {
   debugger
-  
   var message = data.val();
   displayComments(message.message);
 });
   
 function displayComments(message){
-  debugger
   
   var listItem = document.createElement("p");
   listItem.textContent = message;
